114916af61d4535907499e579839f3369b106445
Peter H. Boling <peter.boling@gmail.com>
2025-07-14 12:34:45 +0000

    👷 Turn code coverage back on

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,15 +13,15 @@ require "config/byebug"
 require "config/rspec/rspec_block_is_expected"
 require "config/rspec/rspec_core"
 
-# # Last thing before loading this gem is to setup code coverage
-# begin
-#   # This does not require "simplecov", but
-#   require "kettle-soup-cover"
-#   #   this next line has a side-effect of running `.simplecov`
-#   require "simplecov" if defined?(Kettle::Soup::Cover) && Kettle::Soup::Cover::DO_COV
-# rescue LoadError
-#   nil
-# end
+# Last thing before loading this gem is to setup code coverage
+begin
+  # This does not require "simplecov", but
+  require "kettle-soup-cover"
+  #   this next line has a side-effect of running `.simplecov`
+  require "simplecov" if defined?(Kettle::Soup::Cover) && Kettle::Soup::Cover::DO_COV
+rescue LoadError
+  nil
+end
 
 # This library
 require "support/acceptance_test_helpers"
