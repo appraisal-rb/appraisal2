9c1d1732331f65214726addb61467eac4f6d3453
Peter H. Boling <peter.boling@gmail.com>
2026-06-22 13:39:58 -0600

    🎨 Template bootstrap by kettle-jem v7.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,10 +21,10 @@ begin
   require "kettle-soup-cover"
   if Kettle::Soup::Cover::DO_COV
     require "simplecov"
+    require "kettle/soup/cover/config"
     SimpleCov.start
   end
   #   this next line has a side-effect of running `.simplecov`
-  require "simplecov" if defined?(Kettle::Soup::Cover) && Kettle::Soup::Cover::DO_COV
 rescue LoadError
   # check the error message and re-raise when unexpected
   nil
