5b845ff055992d71221e96cde6dcb067600e58d8
Peter H. Boling <peter.boling@gmail.com>
2026-08-06 18:41:40 -0600

    🔖 Bump gem version

--- lib/appraisal/version.rb
+++ lib/appraisal/version.rb
@@ -4,7 +4,7 @@ module Appraisal
   # Version namespace for this gem.
   module Version
     # Current gem version.
-    VERSION = "3.2.1"
+    VERSION = "3.2.2"
   end
   # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
